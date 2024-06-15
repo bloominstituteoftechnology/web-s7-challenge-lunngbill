eb7c278db7b61a2022997a3b8764f1d4cdbd96ca
Lun Ng <lunngbill@gmail.com>
2024-06-15 15:33:48 -0700

    finished

--- mvpB.test.js
+++ mvpB.test.js
@@ -3,35 +3,71 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 describe('Sprint 7 Challenge Learner Tests', () => {
-  /*
-  👉 TASK 1 - Unit Testing of sum function at the bottom of this module
+  
+  /* 👉 TASK 1 - Unit Testing of sum function at the bottom of this module 
 
-  Test the following. You can create separate tests or a single test with multiple assertions.
+  Test the following. You can create separate tests or a single test with multiple assertions.*/
+    
+    test('[1] sum() // throws an error "pass valid numbers"', () => {
+      expect(() => sum()).toThrow('pass valid numbers')
+    })
 
-    [1] sum() // throws an error 'pass valid numbers'
-    [2] sum(2, 'seven') // throws an error 'pass valid numbers'
-    [3] sum(1, 3) // returns 4
-    [4] sum('1', 2) // returns 3
-    [5] sum('10', '3') // returns 13
-  */
+    test('[2] sum(2, "seven") // throws an error "pass valid numbers"', () => {
+      expect(() => sum(2, 'seven')).toThrow('pass valid numbers')
+    })
+    
+    test('[3] sum(1, 3) // returns 4', () => {
+      expect(sum(1, 3)).toEqual(4)
+    })
 
-  /*
-  👉 TASK 2 - Integration Testing of HelloWorld component at the bottom of this module
+    test('[4] sum("1", 2) // returns 3', () => {
+      expect(sum('1', 2)).toEqual(3)
+    })
+
+    test('[5] sum("10", "3") // returns 13', () => {
+      expect(sum('10', '3')).toEqual(13)
+    })
+  
+  /* 👉 TASK 2 - Integration Testing of HelloWorld component at the bottom of this module 
 
   Test the <HelloWorld /> component found below...
     - using `screen.queryByText` to capture nodes
-    - using `toBeInTheDocument` to assert their existence in the DOM
+    - using `toBeInTheDocument` to assert their existence in the DOM */
 
-    [1] renders a link that reads "Home"
-    [2] renders a link that reads "About"
-    [3] renders a link that reads "Blog"
-    [4] renders a text that reads "The Truth"
-    [5] renders a text that reads "JavaScript is pretty awesome"
-    [6] renders a text that includes "javaScript is pretty" (use exact = false)
-  */
-  test('you can comment out this test', () => {
+    
+    test('[1] renders a link that reads "Home"', () => {
+      render(<HelloWorld/>)
+      expect(screen.queryByText('Home')).toBeInTheDocument()
+    })
+
+    test('[2] renders a link that reads "About"', () => {
+      render(<HelloWorld/>)
+      expect(screen.queryByText('About')).toBeInTheDocument()
+    })
+
+    test('[3] renders a link that reads "Blog"', () => {
+      render(<HelloWorld/>)
+      expect(screen.queryByText('Blog')).toBeInTheDocument()
+    })
+
+    test('[4] renders a text that reads "The Truth"', () => {
+      render(<HelloWorld/>)
+      expect(screen.queryByText("The Truth")).toBeInTheDocument()
+    })
+
+    test('[5] renders a text that reads "JavaScript is pretty awesome"', () => {
+      render(<HelloWorld/>)
+      expect(screen.queryByText("JavaScript is pretty awesome")).toBeInTheDocument()
+    })
+
+    test('[6] renders a text that includes "javaScript is pretty" (use exact = false)', () => {
+      render(<HelloWorld/>)
+      expect(screen.queryByText(/javaScript is pretty/i)).toBeInTheDocument()
+    })
+  
+  /*test('you can comment out this test', () => {
     expect(true).toBe(false)
-  })
+  }) */
 })
 
 function sum(a, b) {
